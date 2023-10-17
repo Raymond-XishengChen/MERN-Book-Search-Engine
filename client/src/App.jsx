2e41de4cb104c07cdaefa1d9a652c79970622907
RaymondChen <xi.chen2210@gmail.com>
2023-10-17 23:36:34 +1100

    Updated connection and server js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,42 @@ import './App.css';
 import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
+import SearchBooks from "./pages/SearchBooks";
+import SavedBooks from "./pages/SavedBooks";
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollp-boost";
+
+const client = new Apolloclient({
+  request: (operation) => {
+    const token = localStorage.getItem("id_token");
+    
+    operation.setContext({
+      headers:{
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    });
+  },
+  uri: "/graphql",
+})
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+
+          <Routes>
+            <Route path="/" element={<SearchBooks />} />
+            <Route path="/saved" element={<SavedBooks />} />
+            <Route
+              path="*"
+              element={<h1 className="display-2">Wrong page!</h1>}
+            />
+          </Routes>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
